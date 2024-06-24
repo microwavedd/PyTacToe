db5785428c66f200676d97febd30c698228a17a7
Microondas <143319861+microwavedd@users.noreply.github.com>
2024-06-24 12:24:05 +0200

    Add files via upload

--- tictactoe.py
+++ tictactoe.py
@@ -2,15 +2,24 @@ import sys
 import pygame
 import numpy as np
 
+#Constants
+
 W,H,R,C = 500,500,3,3
 sizeSquare = W // C
 backgroundColor = (28,170,156)
 lineColor = (23,45,75)
+circColor = (239,231,200)
+circWidth = 15
+radius = sizeSquare // 4
 lineWidth = 15
+crossColor = (66,66,66)
+crossWidth = 20
+offset = 50
+
 
 pygame.init()
 screen = pygame.display.set_mode( (W,H) )
-pygame.display.set_caption("TIC TAC TOE")
+pygame.display.set_caption("PyTacToe")
 screen.fill(backgroundColor)
 class Board:
     def __init__(self):
@@ -37,6 +46,18 @@ class Back:
         pygame.draw.line(screen, lineColor, (0, H*(2/3)), (W, H*(2/3)), lineWidth)
     def changeplayer(self):
         self.player = self.player % 2 + 1
+        
+    def draw_fig(self,row,col):
+        if self.player == 1:
+            startDown = (col * sizeSquare + offset, row * sizeSquare + offset)
+            endDown = (col * sizeSquare + sizeSquare - offset, row * sizeSquare + sizeSquare - offset)
+            pygame.draw.line(screen,crossColor, startDown, endDown, crossWidth)
+            startUp = (col * sizeSquare + offset, row * sizeSquare + sizeSquare - offset)
+            endUp = (col * sizeSquare + sizeSquare - offset, row * sizeSquare + offset)
+            pygame.draw.line(screen,crossColor,startUp,endUp,crossWidth)
+        elif self.player == 2:
+            center = (col * sizeSquare + sizeSquare // 2, row * sizeSquare + sizeSquare // 2)
+            pygame.draw.circle(screen,circColor,center,radius,circWidth)
     
 
 def main():
@@ -56,9 +77,9 @@ def main():
                 col = pos[0] // sizeSquare
                 if gboard.empty(row,col):
                     gboard.mark(row,col, game.player)
+                    game.draw_fig(row,col)
                     game.changeplayer()
-                    print(gboard.squares)
-                
+                print(gboard.squares)
         pygame.display.update()
 main()
         
